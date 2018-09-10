e271f8fa9c20be42a2b124261dc3c31dc51a35f0
xuanyuan <z_dianjun@163.com>
2018-09-10 19:34:27 +0800

    finish development chapter

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  /*
+  * webpack-dev-server提供的简单web服务器，能够实时重新加载
+  * 修改配置文件，告诉开发服务器，在哪里查找文件
+  * 此处配置告知 webpack-dev-server，在localhost:8080下建立服务，
+  * 将dist目录下的文件， 作为可访问文件。
+  */
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    // 每次构建前清理dist文件夹
+    new CleanWebpackPlugin(['dist']),
+    // 创建全新的index.html,所有的bundle会自动添加到html中
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
