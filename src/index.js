c6dc6474bad64dd77e72ea42262ee087f8db2fc7
xuanyuan <z_dianjun@163.com>
2018-09-10 19:01:53 +0800

    finish resource management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from "lodash";
+import './style.css';
+import Icon from './icon.png';
+
+
 function component() {
   var element = document.createElement('div');
   
   // lodash
   element.innerHTML = _.join(["hello", "webpack"], ' ');
- 
+  element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
   return element;
 }
 document.body.appendChild(component());
